d681cde91358be82bca915fb5d9ea74a71dc1aa9
nildontsleep <132806671+nildontsleep@users.noreply.github.com>
2025-01-09 14:14:10 +0100

    🏯

--- install.bat
+++ install.bat
@@ -1,3 +1,16 @@
 @echo off
+title Kinoflix | Installer
+echo Installing modules for Kinoflix !
+echo  - Flask
+echo  - Pystyle
+echo  - PyYAML
+echo  - Werkzeug Utils
+timeout 3 > NUL
+cls
 python -m pip install flask pystyle pyyaml werkzeug
-pause
+cls
+echo Everything should have installed successfuly. You can now run the app.py through CMD.
+echo If you need any further help ; refer to readme : https://github.com/nildontsleep/kinoflix
+echo.
+echo Press any key to exit the modules installer
+pause > NUL
